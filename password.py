d8b4f0792cd10b4ffc2020c974271b59f67599b5
kkmanwork <jabawork015@gmail.com>
2023-05-30 17:21:03 +0800

    basic version done

--- password.py
+++ password.py
@@ -2,10 +2,13 @@ password = "a123456"
 i = 3
 
 while i >= 0:
+    i = i-1
     test = input("請輸入密碼:")
     if password == test:
         print("登入成功")
         break
     else:
-        i = i-1
+        if i == 0:
+            print("沒機會嘗試了")
+            break
         print("密碼錯誤! 還有", i, "次機會")
